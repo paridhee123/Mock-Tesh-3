2affc98f4102ab55e6b23d6ede30e7f4d0ffc816
paridhee123 <38458413+paridhee123@users.noreply.github.com>
2023-06-26 01:02:48 +0530

    Create PythonStack.py

---
+++ PythonStack.py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            raise IndexError("Stack is empty. Cannot pop element.")
+
+    def is_empty(self):
+        return len(self.stack) == 0
